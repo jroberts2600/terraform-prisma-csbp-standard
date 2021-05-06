f9d4c20ecd5cb80f3d30282e55f0afd863e87d14
Joe Buhr <jtb75@users.noreply.github.com>
2021-05-06 14:04:51 -0500

    .

--- csbp.tf
+++ csbp.tf
@@ -1,17 +1,31 @@
-resource "prismacloud_compliance_standard_requirement_section" "s3" {
-    csr_id = prismacloud_compliance_standard_requirement.encryption_requirements.csr_id
-    section_id = "S3"
-    description = "S3 Encryption Controls"
-}
-
-resource "prismacloud_compliance_standard_requirement" "encryption_requirements" {
-    cs_id = prismacloud_compliance_standard.csbp_standard.cs_id
-    name = "Encryption"
-    description = "Controls for Ensuring Data Encryption"
-    requirement_id = "1.0"
-}
-
 resource "prismacloud_compliance_standard" "csbp_standard" {
     name = "AAA Cloud Security Standards"
     description = "Cloud Security Standards for AAA Organization"
 }
+
+########################################################################################
+# Storage Requirements
+########################################################################################
+resource "prismacloud_compliance_standard_requirement" "storage_reqs" {
+    cs_id = prismacloud_compliance_standard.csbp_standard.cs_id
+    name = "Storage"
+    description = "Storage Governance Controls"
+    requirement_id = "1.0"
+}
+
+    resource "prismacloud_compliance_standard_requirement_section" "access" {
+        csr_id = prismacloud_compliance_standard_requirement.storage_reqs.csr_id
+        section_id = "Access"
+        description = "Public and Private Storage Access Controls"
+    }
+
+    resource "prismacloud_compliance_standard_requirement_section" "encryption" {
+        csr_id = prismacloud_compliance_standard_requirement.storage_reqs.csr_id
+        section_id = "Encryption"
+        description = "Encryption Controls"
+    }
+
+
+########################################################################################
+# Network Security Requirements
+########################################################################################
