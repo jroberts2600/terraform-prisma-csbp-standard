0a7ed651d9d7a24aa8b32ceb3cc9ad824d8b225d
Joe Buhr <jtb75@users.noreply.github.com>
2021-05-06 14:28:27 -0500

    .

--- csbp.tf
+++ csbp.tf
@@ -18,13 +18,13 @@ resource "prismacloud_compliance_standard_requirement" "storage_reqs" {
         section_id = "Access"
         description = "Public and Private Storage Access Controls"
     }
-
+/*
     resource "prismacloud_compliance_standard_requirement_section" "s_encryption" {
         csr_id = prismacloud_compliance_standard_requirement.storage_reqs.csr_id
         section_id = "Encryption"
         description = "Encryption Controls"
     }
-
+*/
 
 ########################################################################################
 # Network Security Requirements
